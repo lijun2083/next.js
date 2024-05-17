4fc93802b91ce8d908487bed5f89c9f9bd95dfca
lijun2083 <lijun@izhemo.com>
2024-05-17 10:21:58 +0800

    Delete examples directory

--- examples/with-apollo/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ApolloClientProvider, RepoList } from "@/client-components";
-
-export default async function Home() {
-  return (
-    <ApolloClientProvider>
-      <RepoList />
-    </ApolloClientProvider>
-  );
-}
