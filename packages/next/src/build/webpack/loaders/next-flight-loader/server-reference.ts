fdd8a2e20b8e7312896a9d8698ae394cf2c38b0f
lijun2083 <lijun@izhemo.com>
2024-05-17 10:22:09 +0800

    Delete packages directory

--- packages/next/src/build/webpack/loaders/next-flight-loader/server-reference.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { registerServerReference as flightRegisterServerReference } from 'react-server-dom-webpack/server.edge'
-
-export function registerServerReference(id: string, action: any) {
-  return flightRegisterServerReference(action, id, null)
-}
